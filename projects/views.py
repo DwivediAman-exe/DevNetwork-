09a2451cc6e765ce928fce5fa3c227d8e5dad531
Aman Dwivedi <19dwivediaman@gmail.com>
2021-07-17 23:05:48 +0530

    project setup

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def projects(request):
+	return HttpResponse('here is the project page')
+
+def project(request, pk):
+	return HttpResponse('single project' + ' ' + pk)
