1b7191a2c33fccac66ca270e0523e1c7283a6cf4
Aman Dwivedi <19dwivediaman@gmail.com>
2021-07-18 10:34:12 +0530

    rendering templates

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-projectList
+projectList = [
+    {
+        'id': '1',
+        'title': 'commerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'portfolio Website',
+        'description': 'Project build by me'
+    },
+    {
+        'id': '3',
+        'title': 'social Website',
+        'description': 'open source website'
+    }
+]
+
 
 def projects(request):
-	page = 'projects'
-	number = 11
-	context = {'page': page, 'number': number}
-	return render(request, 'projects/projects.html', context)
+    page = 'projects'
+    number = 11
+    context = {'page': page, 'number': number, 'projects': projectList}
+    return render(request, 'projects/projects.html', context)
+
 
 def project(request, pk):
-	return render(request, 'projects/single-project.html')
+    projectObj = None
+    for i in projectList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project': projectObj})
