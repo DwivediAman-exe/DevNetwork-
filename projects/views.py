40673f180cf6a127c0201b3f1790f9cbbfc578e8
Aman Dwivedi <19dwivediaman@gmail.com>
2021-07-18 10:11:53 +0530

    [ractise

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectList
+
 def projects(request):
-	return HttpResponse('here is the project page')
+	page = 'projects'
+	number = 11
+	context = {'page': page, 'number': number}
+	return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-	return HttpResponse('single project' + ' ' + pk)
+	return render(request, 'projects/single-project.html')
